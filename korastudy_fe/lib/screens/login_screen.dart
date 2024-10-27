7293a01bf9946bec0c180dbec8ad08bcbb0167b8
wadashinichi <edogawa280723@gmail.com>
2024-10-27 10:01:13 +0700

    update login and register screen

--- korastudy_fe/lib/screens/login_screen.dart
+++ korastudy_fe/lib/screens/login_screen.dart
@@ -24,105 +24,113 @@ class _LoginScreenState extends State<LoginScreen> {
         centerTitle: true,
         backgroundColor: Color(0xFF1EA5FC),
       ),
-      body: Expanded(
-        child: Padding(
-          padding: const EdgeInsets.all(35.0),
-          child: Column(
-            children: [
-              Image.asset('assets/images/bg.png'),
-              SizedBox(
-                height: 16.0,
-              ),
-              Container(
-                child: LoginInput(label: "Email", icon: Icons.email),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                child: LoginInput(label: "Mật khẩu", icon: Icons.lock),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    "Quên mật khẩu?",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Color(0xFF1EA5FC),
-                        elevation: 5,
-                        shadowColor: Colors.black,
-                      ),
-                      onPressed: () {},
-                      child: Text(
-                        "Đăng nhập",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton.icon(
-                      onPressed: () {},
-                      label: Text(
-                        "Đăng nhập bằng Google",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                      icon: Icon(
-                        Icons.person,
-                        color: Colors.black,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFEEEEEE), elevation: 5),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(35.0),
+            child: Column(
+              children: [
+                Image(image: AssetImage('assets/images/logo.png')),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Container(
+                  child: LoginInput(label: "Email", icon: Icons.email),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  child: LoginInput(label: "Mật khẩu", icon: Icons.lock),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text("Bạn chưa có tài khoản?"),
-                    SizedBox(
-                      width: 8,
-                    ),
                     Text(
-                      "Đăng ký",
-                      style: TextStyle(
-                        color: Color(0xFF1EA5FC),
-                        decoration: TextDecoration.underline,
+                      "Quên mật khẩu?",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Color(0xFF1EA5FC),
+                          elevation: 5,
+                          shadowColor: Colors.black,
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Đăng nhập",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        onPressed: () {},
+                        label: Text(
+                          "Đăng nhập bằng Google",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        icon: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFFEEEEEE), elevation: 5),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Bạn chưa có tài khoản?"),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "Đăng ký",
+                        style: TextStyle(
+                          color: Color(0xFF1EA5FC),
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- korastudy_fe/lib/screens/register_screen.dart
+++ korastudy_fe/lib/screens/register_screen.dart
@@ -24,124 +24,137 @@ class _RegisterScreenState extends State<RegisterScreen> {
         centerTitle: true,
         backgroundColor: Color(0xFF1EA5FC),
       ),
-      body: Expanded(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/images/bg.png')),
+        ),
         child: Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(35.0),
-            child: Column(
-              children: [
-                Image.asset('assets/images/bg.png'),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Container(
-                  child: LoginInput(label: "Nhập tên", icon: Icons.person),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-                  child: LoginInput(label: "Nhập email", icon: Icons.email),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-                  child: LoginInput(label: "Nhập mật khẩu", icon: Icons.lock),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-                  child:
-                      LoginInput(label: "Xác nhận mật khẩu", icon: Icons.lock),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Checkbox(
-                      value: true,
-                      onChanged: (bool? value) {},
-                      activeColor: Colors.black,
-                    ),
-                    Text(
-                      "Lưu tài khoản",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Color(0xFF1EA5FC),
-                          elevation: 5,
-                          shadowColor: Colors.black,
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "Đăng ký",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton.icon(
-                        onPressed: () {},
-                        label: Text(
-                          "Đăng nhập bằng Google",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        icon: Icon(
-                          Icons.person,
-                          color: Colors.black,
-                        ),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xFFEEEEEE), elevation: 5),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+          child: Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(35.0),
+              child: ListView(
+                children: [
+                  Column(
                     children: [
-                      Text("Bạn đã có tài khoản?"),
+                      Image.asset('assets/images/logo.png'),
                       SizedBox(
-                        width: 8,
+                        height: 16.0,
                       ),
-                      Text(
-                        "Đăng nhập",
-                        style: TextStyle(
-                          color: Color(0xFF1EA5FC),
-                          decoration: TextDecoration.underline,
+                      Container(
+                        child:
+                            LoginInput(label: "Nhập tên", icon: Icons.person),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        child:
+                            LoginInput(label: "Nhập email", icon: Icons.email),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        child: LoginInput(
+                            label: "Nhập mật khẩu", icon: Icons.lock),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        child: LoginInput(
+                            label: "Xác nhận mật khẩu", icon: Icons.lock),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Checkbox(
+                            value: true,
+                            onChanged: (bool? value) {},
+                            activeColor: Colors.black,
+                          ),
+                          Text(
+                            "Lưu tài khoản",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: Color(0xFF1EA5FC),
+                                elevation: 5,
+                                shadowColor: Colors.black,
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                "Đăng ký",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
+                              label: Text(
+                                "Đăng nhập bằng Google",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              icon: Icon(
+                                Icons.person,
+                                color: Colors.black,
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color(0xFFEEEEEE),
+                                  elevation: 5),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Bạn đã có tài khoản?"),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              "Đăng nhập",
+                              style: TextStyle(
+                                color: Color(0xFF1EA5FC),
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
