2d3bcf25e3a74e6b7f2a84951e9d0008abb6a3b6
ThienTDV <thienxauxi1@gmail.com>
2024-11-01 22:57:45 +0700

    new update cua thanh search input

--- korastudy_fe/lib/widgets/search_input.dart
+++ korastudy_fe/lib/widgets/search_input.dart
@@ -15,7 +15,7 @@ class _SearchInputState extends State<SearchInput> {
       child: TextField(
         decoration: InputDecoration(
           contentPadding: EdgeInsets.symmetric(
-              vertical: 8, horizontal: 12), // Điều chỉnh padding bên trong
+              vertical: 8, horizontal: 15), // Điều chỉnh padding bên trong
           hintText: "Tìm kiếm...",
           hintStyle: TextStyle(fontSize: 14), // Kích thước chữ của placeholder
           border: OutlineInputBorder(
@@ -23,17 +23,20 @@ class _SearchInputState extends State<SearchInput> {
             borderSide: BorderSide(color: Color(0xFFA5A5A5)),
           ),
           suffixIcon: Padding(
-            padding: const EdgeInsets.all(4.0), // Thêm padding quanh icon
+            padding: const EdgeInsets.all(5), // Reduced padding around the icon
             child: Container(
+              width: 30, // Set a smaller width for the container
+              height: 30, // Set a smaller height for the container
               decoration: BoxDecoration(
                 color: Color(0xFF1EA5FC),
                 shape: BoxShape.circle,
               ),
               child: IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.search,
-                    size: 20), // Kích thước biểu tượng nhỏ hơn
+                icon: Icon(Icons.search, size: 18), // Smaller icon size
                 color: Colors.white,
+                padding:
+                    EdgeInsets.zero, // Remove default padding for the button
               ),
             ),
           ),
